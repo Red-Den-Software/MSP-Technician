7121540934dbd68bbc37bb207c8dc0d23429fe94
R3ZJ <seth@redden.dev>
2026-05-12 15:22:41 -0400

    Changing Main

--- App.xaml.cs
+++ App.xaml.cs
@@ -10,18 +10,7 @@ namespace Data_Transfer_App
     /// </summary>
     /// using Velopack;
 
-    internal static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            VelopackApp.Build().Run();
-
-            var app = new App();
-            app.InitializeComponent();
-            app.Run();
-        }
-    }
+   
     public partial class App : System.Windows.Application
     {
       public async Task CheckForUpdatesAsync()
