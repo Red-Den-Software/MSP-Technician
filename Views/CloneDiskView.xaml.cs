ecb6829bf0496b08d4fa9666b7fdb0f35ba1deb5
MrRESJ <sethjredden@gmail.com>
2026-05-26 22:39:46 -0400

    Added in Models and manufacturers

--- Views/CloneDiskView.xaml.cs
+++ Views/CloneDiskView.xaml.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.Win32;
+using msptool.Button_Commands;
 using msptool.ViewModels;
 using System;
-using System.IO;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Management;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Forms;
@@ -17,19 +21,25 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.Forms.MessageBox;
+using RadioButton = System.Windows.Controls.RadioButton;
 namespace msptool.Views
 {
     public partial class CloneDiskView : System.Windows.Controls.UserControl
     {
-       
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         public CloneDiskView()
         {
            
             InitializeComponent();
-            createButtons();
+            CreateButtons();
 
         }
+        private string _src_disk;
+        
         public List<string> GetDiskInfo()
         {
             List<string> drives = new List<string>();
@@ -40,28 +50,105 @@ namespace msptool.Views
             {
                 if (d.IsReady)
                 {
-                    drives.Add(d.Name);
+                    string model = GetDriveModel(d.Name);
+
+                    string brand = GetBrand(model);
+
+                    drives.Add($"{brand} {model} ({d.Name})");
                 }
             }
 
             return drives;
         }
-        public void createButtons()
+        
+        public void CreateButtons()
         {
             List<string> driveLetters = GetDiskInfo();
 
             foreach (string drive in driveLetters)
             {
-                System.Windows.Controls.Button driveButton = new System.Windows.Controls.Button();
-                driveButton.Style = (Style)TryFindResource("cloneDiskBut");
+                System.Windows.Controls.RadioButton driveButton = new System.Windows.Controls.RadioButton();
+               driveButton.GroupName = "Drives";
                 driveButton.Content = drive;
-                driveButton.Height = 25;
-                driveButton.Width = 296;
-
+                driveButton.Style = (Style)TryFindResource("cloneDiskBut");
+                driveButton.Tag = drive;
+                driveButton.Click += DriveButton_Click;
                 ButtonPanelClone.Children.Add(driveButton);
             }
         }
+        private void DriveButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.RadioButton button = sender as RadioButton;
 
+            string drive = button.Tag.ToString();
+            CloneDisk cloneDisk = new CloneDisk();
+           // cloneDisk.Setup(drive);
+
+            MessageBox.Show($"Selected Drive: {drive}");
+        }
+        private string GetDriveModel(string driveLetter)
+        {
+            try
+            {
+                string letter = driveLetter.Replace("\\", "");
+
+                using (ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher(
+                        "SELECT * FROM Win32_LogicalDiskToPartition"))
+                {
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        string dependent = obj["Dependent"].ToString();
+
+                        if (dependent.Contains(letter))
+                        {
+                            string antecedent = obj["Antecedent"].ToString();
+
+                            string partitionId = antecedent.Split('"')[1];
+
+                            using (ManagementObjectSearcher diskSearcher =
+                                new ManagementObjectSearcher(
+                                    "ASSOCIATORS OF {Win32_DiskPartition.DeviceID='" +
+                                    partitionId +
+                                    "'} WHERE AssocClass = Win32_DiskDriveToDiskPartition"))
+                            {
+                                foreach (ManagementObject disk in diskSearcher.Get())
+                                {
+                                    return disk["Model"]?.ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
+
+            return "Unknown Drive";
+        }
+        private string GetBrand(string model)
+        {
+            if (model.StartsWith("CT"))
+                return "Crucial";
+
+            if (model.StartsWith("WDC"))
+                return "Western Digital";
+
+            if (model.StartsWith("Samsung"))
+                return "Samsung";
+
+            if (model.StartsWith("ST"))
+                return "Seagate";
+
+            if (model.StartsWith("KINGSTON"))
+                return "Kingston";
+
+            if (model.StartsWith("SanDisk"))
+                return "SanDisk";
+
+            return "Unknown";
+        }
     }
 
 }
